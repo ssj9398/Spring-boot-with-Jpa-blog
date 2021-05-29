cc33306a9935e5ec1aba69c062c2ba43484c79e0
alice9398 <sjs939883@gmail.com>
2021-05-29 21:30:29 +0900

    github test

---
+++ Hello/src/HelloWord.java
@@ -0,0 +1,13 @@
+
+//이클립스 = Ctrl +F11
+//KeyMap = 인텔리 J  =  Shift + F10
+public class HelloWord {
+
+	//스프링부트 연습 테스트
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");
+System.out.println("Hello");
+	}
+
+}
