770a8c16eb4e3d4bfe3db065086a6b158520492f
alice9398 <sjs939883@gmail.com>
2021-05-30 23:29:55 +0900

    Lombok 세팅 및 테스트 (Getter,Setter,data,Builder)

--- blog/src/main/java/com/alisyabob/blog/test/HttpControllerTest.java
+++ blog/src/main/java/com/alisyabob/blog/test/HttpControllerTest.java
@@ -15,6 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpControllerTest {
 	
+	private static final String TAG = "HttpControllerTest : ";
+	
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		//Member m = new Member(1, "aaa", "1234", "a@a.com");
+		Member m = Member.builder().username("one").password("1234").email("a@naver.com").build();
+		System.out.println(TAG + "getter : " +m.getId());
+		m.setId(5000);
+		System.out.println(TAG + "setter : "+m.getId() );
+		return "lombok test 완료";
+	}
+	
 	/*get 방식*/
 	//브라우저 요청은 get밖에 할 수 없다. get은 무조건 queryString으로 보낸다.
 	//http://localhost:8080/http/get
@@ -25,6 +37,7 @@ public class HttpControllerTest {
 	//public String getTest(@RequestParam int id) {     
 	//한번에
 	public String getTest(Member m) {
+		
 		return "get 요청"+m.getId()+m.getUsername()+m.getEmail()+m.getPassword();
 	}
 	
--- blog/src/main/java/com/alisyabob/blog/test/Member.java
+++ blog/src/main/java/com/alisyabob/blog/test/Member.java
@@ -1,44 +1,33 @@
 package com.alisyabob.blog.test;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+//@Getter
+//@Setter
+@Data
+
+//모든페이지를 다쓰느는 생성자 만들기
+//@AllArgsConstructor
+// 빈생성자
+@NoArgsConstructor
 public class Member {
 	private int id;
 	private String username;
 	private String password;
 	private String email;
-	public int getId() {
-		return id;
-	}
 	
-	
-	
+	@Builder
 	public Member(int id, String username, String password, String email) {
+		super();
 		this.id = id;
 		this.username = username;
 		this.password = password;
 		this.email = email;
 	}
-
-
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
 }
