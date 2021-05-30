ad6a123c82518571178cf32332f2d552b4bd136a
alice9398 <sjs939883@gmail.com>
2021-05-30 22:16:54 +0900

    get, post, put 방식 설명 및 예시

--- blog/src/main/java/com/alisyabob/blog/test/HttpControllerTest.java
+++ blog/src/main/java/com/alisyabob/blog/test/HttpControllerTest.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //사용자가 요청 시 응답(HTML파일)
@@ -13,20 +15,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpControllerTest {
 	
-	//브라우저 요청은 get밖에 할 수 없다.
+	/*get 방식*/
+	//브라우저 요청은 get밖에 할 수 없다. get은 무조건 queryString으로 보낸다.
 	//http://localhost:8080/http/get
+	
 	@GetMapping("/http/get")
-	public String getTest() {
-		return "get 요청";
+	
+	//단일
+	//public String getTest(@RequestParam int id) {     
+	//한번에
+	public String getTest(Member m) {
+		return "get 요청"+m.getId()+m.getUsername()+m.getEmail()+m.getPassword();
 	}
 	
-	@PostMapping("/http/post")
-	public String postTest() {
-		return "post 요청";
+	
+	/*post 방식*/
+	@PostMapping("/http/post")// application/json
+	public String postTest(@RequestBody Member m) {
+		return "post 요청"+m.getId()+m.getUsername()+m.getEmail()+m.getPassword();
 	}
 	
+	
+	/*put 방식*/
+	//RequestBody를 통해 값을 받는다.
 	@PutMapping("/http/put")
-	public String putTest() {
+	public String putTest(@RequestBody Member m) {
 		return "put 요청";
 	}
 	
