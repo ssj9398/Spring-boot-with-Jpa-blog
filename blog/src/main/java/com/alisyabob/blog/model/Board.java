9f69c47861e69e25417364d13a3b84ee5f89af65
alice9398 <sjs939883@gmail.com>
2021-06-17 00:23:33 +0900

    답변부분 추가 및 fetch타입추가

--- blog/src/main/java/com/alisyabob/blog/model/Board.java
+++ blog/src/main/java/com/alisyabob/blog/model/Board.java
@@ -1,15 +1,18 @@
 package com.alisyabob.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -40,9 +43,12 @@ public class Board {
 	@ColumnDefault("0")
 	private int count;  //조회수
 
-	@ManyToOne  //Many = Board, User = One
+	@ManyToOne(fetch = FetchType.EAGER)  //Many = Board, User = One
 	@JoinColumn(name="userId")
 	private User user; //DB는 오브젝트를 저장할 수 없다. FK , 자바는 오브젝트를 저장할 수 있다.
+	
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER) //mappedBy 연관관계의 주인이 아니다. (FK x) 컬럼 생성 x
+	private List<Reply> reply;
 	
 	@CreationTimestamp
 	private Timestamp createDate;
