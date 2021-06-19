5b35d91475626430c144ce8ae9563b23a0580cf7
alice9398 <sjs939883@gmail.com>
2021-06-19 23:03:07 +0900

    id로 select 테스트(IllegalArgumentException)

--- blog/src/main/java/com/alisyabob/blog/test/DummyControllerTest.java
+++ blog/src/main/java/com/alisyabob/blog/test/DummyControllerTest.java
@@ -1,6 +1,10 @@
 package com.alisyabob.blog.test;
 
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +18,23 @@ public class DummyControllerTest {
 	//의존성 주입 DI
 	@Autowired
 	private UserRepository userRepository;
+	
+	//{id} 주소로 파라메터를 전달 받기
+	//http://localhost:8080/blog/dummy/user/3
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		
+		//없는 값을 를 찾았을때 null이 될때 Optional로 User객체를 감싸서 가져와서 null인지 아닌지 판단해서 return
+		
+		//빈객체생성
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+		@Override
+		public IllegalArgumentException get() {
+			return new IllegalArgumentException("해당 유저는 없습니다. id:"+id);
+		}
+	});
+		return user;
+	}
 	
 	//http://localhost:8080/blog/dummy/join(요청)
 	//http의 body에 username, password, email 데이터를 가지고(요청)
