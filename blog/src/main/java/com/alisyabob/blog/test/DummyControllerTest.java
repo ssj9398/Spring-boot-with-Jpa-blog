99a9ea82a899c235f1c4a1ace6385432d5761f8a
alice9398 <sjs939883@gmail.com>
2021-06-20 21:10:47 +0900

    Delete

--- blog/src/main/java/com/alisyabob/blog/test/DummyControllerTest.java
+++ blog/src/main/java/com/alisyabob/blog/test/DummyControllerTest.java
@@ -6,10 +6,12 @@ import java.util.function.Supplier;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,6 +30,19 @@ public class DummyControllerTest {
 	//의존성 주입 DI
 	@Autowired
 	private UserRepository userRepository;
+	
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			return "삭제에 실패하였습니다. 해당id는 DB에 없습니다.";
+		}
+		
+		
+		return "삭제되었습니다. id :" +id;
+		
+	}
 	
 	//save함수는 id를 전달하지 않으면 insert를 해주고
 	// id를 전달하면 해당 id에 대한 데이터가 있으면 update
@@ -51,7 +66,7 @@ public class DummyControllerTest {
 		
 		//userRepository.save(user);
 		
-		return null;
+		return user;
 	}
 	
 	@GetMapping("/dummy/users")
