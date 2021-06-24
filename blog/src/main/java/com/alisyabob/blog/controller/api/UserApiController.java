2fdbfbc1374e2d469caf10f1b8db9b976df944bd
alice9398 <sjs939883@gmail.com>
2021-06-24 23:35:19 +0900

    500error 부분 수정

--- blog/src/main/java/com/alisyabob/blog/controller/api/UserApiController.java
+++ blog/src/main/java/com/alisyabob/blog/controller/api/UserApiController.java
@@ -23,6 +23,6 @@ public class UserApiController {
 		// 실제로 DB에 insert를 하고 아래에서 return
 		user.setRole(RoleType.USER);
 		int result = userService.회원가입(user);
-		 return new ResponseDto<Integer>(HttpStatus.OK, result); // 자바오브젝트를 Json으로 변환해서 리턴
+		 return new ResponseDto<Integer>(HttpStatus.OK.value(), result); // 자바오브젝트를 Json으로 변환해서 리턴
 	} 
 }
