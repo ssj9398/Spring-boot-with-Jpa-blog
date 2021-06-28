55eca8b770c820454d5c3ce052ba82cc3347e138
alisyabob <sjs939883@gmail.com>
2021-06-28 23:47:30 +0900

    컨텍스트 제거

--- blog/src/main/resources/static/js/user.js
+++ blog/src/main/resources/static/js/user.js
@@ -24,14 +24,14 @@ let index = {
 		// ajax가 통신을 성공하고 서버가 json을 리턴해주면 자동으로 자바 오브젝트로 변환해줌
 		$.ajax({
 			type: "POST",
-			url: "/blog/api/user",
+			url: "/api/user",
 			data: JSON.stringify(data),  //http body데이터
 			contentType: "application/json; charset=utf-8", //body데이터가 어떤타입인지 (MIME)
 			dataType: "json" //요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열인데 json이라면 javascript object로 변경
 
 		}).done(function(resp) {
 			console.log("회원가입 완료" + resp);
-			location.href = "/blog"
+			location.href = "/"
 		}).fail(function(error) {
 			alert(JSON.stringify(error));
 		});
@@ -44,14 +44,14 @@ let index = {
 		}
 		$.ajax({
 			type: "POST",
-			url: "/blog/api/user/login",
+			url: "/api/user/login",
 			data: JSON.stringify(data),  //http body데이터
 			contentType: "application/json; charset=utf-8", //body데이터가 어떤타입인지 (MIME)
 			dataType: "json" //요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열인데 json이라면 javascript object로 변경
 
 		}).done(function(resp) {
 			alert("로그인 되었습니다.");
-			location.href = "/blog"
+			location.href = "/"
 		}).fail(function(error) {
 			alert(JSON.stringify(error));
 		});
--- blog/src/main/webapp/WEB-INF/views/user/joinForm.jsp
+++ blog/src/main/webapp/WEB-INF/views/user/joinForm.jsp
@@ -15,5 +15,5 @@
 	</form>
 	<button id="btn-save" class="btn btn-primary">회원가입 완료</button>
 </div>
-<script src="/blog/js/user.js"></script>
+<script src="/js/user.js"></script>
 <%@ include file="../layout/footer.jsp"%>
