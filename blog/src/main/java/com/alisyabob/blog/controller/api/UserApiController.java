84492661f70de4daa0124481cfb1f3738237e12c
alisyabob <sjs939883@gmail.com>
2021-06-29 00:13:19 +0900

    spring security 세팅

--- blog/src/main/java/com/alisyabob/blog/controller/api/UserApiController.java
+++ blog/src/main/java/com/alisyabob/blog/controller/api/UserApiController.java
@@ -20,22 +20,24 @@ public class UserApiController {
 	private UserService userService;
 
 	@PostMapping("/api/user")
-	public ResponseDto<Integer> save(@RequestBody User user) {
+	public ResponseDto<Integer> save(@RequestBody User user, HttpSession session) {
 		System.out.println("UserApiController : save 호출됨");
 		// 실제로 DB에 insert를 하고 아래에서 return
 		user.setRole(RoleType.USER);
 		userService.회원가입(user);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바오브젝트를 Json으로 변환해서 리턴
 	}
-
-	@PostMapping("/api/user/login")
-	public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) {
-		System.out.println("UserApiController : login 호출됨");
-		User principal = userService.로그인(user); // principal (접근주체)
-		
-		if(principal!=null) {
-			session.setAttribute("principal", principal);
-		}
-		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
-	}
 }
+
+	
+	/*  로그인은 스프링 시큐리티로 하기위해 주석
+	 * @PostMapping("/api/user/login") 
+	 * public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) {
+	 * System.out.println("UserApiController : login 호출됨"); 
+	 * User principal = userService.로그인(user);  // principal (접근주체)
+	 * if(principal!=null) { 
+	 * session.setAttribute("principal", principal); 
+	 * } 
+	 * return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); }
+	 */
+
